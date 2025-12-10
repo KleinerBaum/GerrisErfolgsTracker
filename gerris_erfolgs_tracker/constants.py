a3393b992696b7ba0a286366cd2d1c7f494a9ed6
GerriF <155729912+KleinerBaum@users.noreply.github.com>
2025-12-10 15:13:29 +0100

    Create constants.py

---
+++ gerris_erfolgs_tracker/constants.py
@@ -0,0 +1,6 @@
+"""Central constants for Streamlit session state keys and defaults."""
+
+SS_TODOS: str = "todos"
+SS_STATS: str = "stats"
+SS_SETTINGS: str = "settings"
+SS_GAMIFICATION: str = "gamification"
